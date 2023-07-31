14649ff30ead23bdb4cf773a840e6c5c943b6350
Jacob Hummer <jcbhmr@outlook.com>
2023-07-31 03:56:33 +0000

    cool

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 #!/usr/bin/env node
 import { DockerDefaultController } from "@runs-on/controller";
-import execWorkflow from "@metactions/exec-workflow";
-import execStep from "@metactions/exec-step";
+import execWorkflow from "@runs-on/exec-workflow";
+import execStep from "@runs-on/exec-step";
 
-const runsOn = core.getInput("runs-on", { required: true });
-
-await $`docker run \
-  --rm \
-  --name ${runsOn} \
-  --platform=linux/amd64 \
-  -dit \
-  ghcr.io/runs-on/${runsOn} \
-  /bin/sh`;
-
-const controller = {
-  spawn(cmd, args = [], options = {}) {
-    const { cwd, env = {}, detached } = options;
-    const envFile = temporaryWriteSync(envStringify(env));
-    return $`docker exec \
-      --env-file ${envFile} \
-      ${cwd ? ["-w", cwd] : []} \
-      ${detached ? ["-d"] : []} \
-      ${runsOn} \
-      ${[cmd].concat(args)}`;
-  },
-};
-
+const controller = new DockerDefaultController("archlinux-*");
 if (process.env.WORKFLOW) {
-  execWorkflow(controller);
+  await execWorkflow(controller);
 } else {
-  execStep(controller);
+  await execStep(controller);
 }
